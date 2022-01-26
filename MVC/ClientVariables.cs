3bd6a5a5ff4e7e9406f3106e24ac30e0d3371ef4
basemelazab <basem.elazab2014@gmail.com>
2022-01-26 16:18:39 +0200

    FirstUpload

---
+++ MVC/ClientVariables.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace MVC
+{
+    public  class ClientVariables
+    {
+        public static HttpClient WebApiClient=new HttpClient();
+
+         static ClientVariables()
+        {
+            WebApiClient.BaseAddress = new Uri("https://localhost:44395/api/");
+            WebApiClient.DefaultRequestHeaders.Accept.Add( new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            WebApiClient.DefaultRequestHeaders.Clear();
+        }
+    }
+}
---
+++ MVC/Controllers/mvcEmployeeController.cs
@@ -0,0 +1,56 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class mvcEmployeeController : Controller
+    {
+        // GET: mvcEmployee
+        public ActionResult Index()
+        {
+            IEnumerable<mvcEmployeeModel> emplist;
+            HttpResponseMessage response = ClientVariables.WebApiClient.GetAsync("Employees").Result;
+            emplist = response.Content.ReadAsAsync<IEnumerable<mvcEmployeeModel>>().Result;
+            return View(emplist);
+        }
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+            { 
+              return View(new mvcEmployeeModel());
+            }
+            else
+            {
+                HttpResponseMessage response = ClientVariables.WebApiClient.GetAsync("Employees/" + id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<mvcEmployeeModel>().Result);
+            }
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(mvcEmployeeModel emp)
+        {
+            if (emp.EmployeeId == 0)
+            { 
+            HttpResponseMessage response = ClientVariables.WebApiClient.PostAsJsonAsync("Employees", emp).Result;
+            TempData["SuccessMessage"] = "Saved Successfully";
+            }
+            else
+            {
+                HttpResponseMessage response = ClientVariables.WebApiClient.PutAsJsonAsync("Employees/" + emp.EmployeeId, emp).Result;
+                TempData["SuccessMessage"] = "Updated Successfully";
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = ClientVariables.WebApiClient.DeleteAsync("Employees/" + id.ToString()).Result;
+            TempData["SuccessMessage"] = "Delete Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
